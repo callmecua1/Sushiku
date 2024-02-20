c136e2609474e2f0b87cc3fe57f60c437fd2762e
callmecua1 <156280593+callmecua1@users.noreply.github.com>
2024-02-20 17:05:36 +0700

    Update review.js

--- src/components/review.js
+++ src/components/review.js
@@ -3,7 +3,7 @@ const review = [
     id: 1,
     name : "Kelvin",
     role : "Food Blogger",
-    img : "./src/assets/review-img/kelvin.jpg",
+    img : "../assets/review-img/kelvin.jpg",
     text: "Sushiku serves fresh and delicious sushi in a friendly atmosphere. The service and food presentation are outstanding. A diverse menu selection makes every visit special.",
   },
 
@@ -11,7 +11,7 @@ const review = [
     id: 2,
     name : "Daniel",
     role : "Youtuber",
-    img : "./src/assets/review-img/daniel.jpg",
+    img : "../assets/review-img/daniel.jpg",
     text: "Sushiku is an authentic ramen spot with a variety of broth and topping options. The ambiance is simple yet comfortable, with fast service and affordable prices. Highly recommended for ramen enthusiasts!",
   },
 
@@ -19,7 +19,7 @@ const review = [
     id: 3,
     name : "Chloe",
     role : "Local Guide",
-    img : "./src/assets/review-img/chloe.jpg",
+    img : "../assets/review-img/chloe.jpg",
     text: "Sushiku offers a unique experience with chefs cooking right in front of you. Delicious dishes, a friendly atmosphere, and entertaining performances. Suitable for families or friends, with great value for money.",
   }
 ];
